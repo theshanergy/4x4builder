7b9c1e0f7e985adee944eb9e187dbd8c12e243dc
Shane Vincent <theshanergy@gmail.com>
2023-04-01 11:03:11 -0700

    memoize ground reflector

--- components/Environment.jsx
+++ components/Environment.jsx
@@ -1,17 +1,27 @@
+import { memo } from 'react'
 import { useThree } from '@react-three/fiber'
 import { Environment, AccumulativeShadows, RandomizedLight, MeshReflectorMaterial, useTexture } from '@react-three/drei'
 import { RepeatWrapping, BackSide } from 'three'
 
-export default function SceneEnvironment({ performanceDegraded }) {
+const Ground = memo(() => {
     const { gl } = useThree()
 
-    // Ground.
+    // Load texture.
     const groundTexture = useTexture('assets/images/ground/ground_tile.png', (texture) => {
         texture.wrapS = texture.wrapT = RepeatWrapping
         texture.repeat.set(228, 228)
         texture.anisotropy = gl.capabilities.getMaxAnisotropy()
     })
 
+    return (
+        <mesh name='Ground' rotation-x={-Math.PI / 2} position={[0, -0.0001, 0]}>
+            <circleGeometry args={[96, 96]} />
+            <MeshReflectorMaterial map={groundTexture} resolution={2048} mirror={0} toneMapped={false} />
+        </mesh>
+    )
+})
+
+export default function SceneEnvironment({ performanceDegraded }) {
     return (
         <>
             <mesh name='Sky'>
@@ -23,10 +33,7 @@ export default function SceneEnvironment({ performanceDegraded }) {
 
             <Environment files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']} path={'assets/images/envmap/'} />
 
-            <mesh name='Ground' rotation-x={-Math.PI / 2} position={[0, -0.0001, 0]}>
-                <circleGeometry args={[96, 96]} />
-                <MeshReflectorMaterial map={groundTexture} resolution={2048} mirror={0} toneMapped={false} />
-            </mesh>
+            <Ground />
 
             {!performanceDegraded && (
                 <AccumulativeShadows temporal scale={10}>
