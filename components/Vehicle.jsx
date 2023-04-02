3ef28630e59fcdf1b10562e940f0842bb9930187
Shane Vincent <theshanergy@gmail.com>
2023-04-01 18:14:03 -0700

    refactor vehicle / wheels, cache calculated tire geometry

--- components/Vehicle.jsx
+++ components/Vehicle.jsx
@@ -1,54 +1,32 @@
-import React, { memo, useMemo, useEffect, useCallback, useRef } from 'react'
+import React, { memo, useMemo, useEffect, useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { Vector3 } from 'three'
 import useAnimateHeight from '../hooks/useAnimateHeight'
 import vehicleConfigs from '../vehicleConfigs'
 import useMaterialProperties from '../hooks/useMaterialProperties'
 
-// Rim.
-const Rim = memo(({ rim, rim_diameter, rim_width, rim_color, rim_color_secondary, color, roughness }) => {
+// Calculate point on line (a to b, at length).
+const linePoint = (a, b, length) => {
+    let dir = b.clone().sub(a).normalize().multiplyScalar(length)
+    return a.clone().add(dir)
+}
+
+// Model loader.
+const Model = ({ path, ...props }) => {
+    const model = useGLTF(path)
+    return <primitive object={model.scene} {...props} />
+}
+
+// Wheels.
+const Wheels = memo(({ rim, rim_diameter, rim_width, rim_color, rim_color_secondary, tire, tire_diameter, offset, wheelbase, axleHeight, color, roughness }) => {
     const { setObjectMaterials } = useMaterialProperties()
 
-    // Load model.
+    // Load models.
     const rimGltf = useGLTF(vehicleConfigs.wheels.rims[rim].model)
-
-    // Create instance.
-    const rimScene = useMemo(() => rimGltf.scene.clone(), [rimGltf.scene])
-
-    // Calculate rim scale as a percentage of diameter.
-    const odScale = useMemo(() => ((rim_diameter * 2.54) / 100 + 0.03175) / vehicleConfigs.wheels.rims[rim].od, [rim, rim_diameter])
-
-    // Calculate rim width.
-    const widthScale = useMemo(() => (rim_width * 2.54) / 100 / vehicleConfigs.wheels.rims[rim].width, [rim, rim_width])
-
-    // Set rim color.
-    useEffect(() => {
-        setObjectMaterials(rimScene, color, roughness, rim_color, rim_color_secondary)
-    }, [rimScene, setObjectMaterials, rim_color, rim_color_secondary, color, roughness])
-
-    return <primitive name='Rim' object={rimScene} scale={[odScale, odScale, widthScale]} />
-})
-
-// Tire
-const Tire = memo(({ rim_diameter, rim_width, tire, tire_diameter }) => {
-    const { setObjectMaterials } = useMaterialProperties(vehicle)
-
-    // Load model.
     const tireGltf = useGLTF(vehicleConfigs.wheels.tires[tire].model)
 
-    // Create instance.
-    const tireScene = useMemo(() => tireGltf.scene.clone(), [tireGltf.scene])
-
-    // Calculate point on line (a to b, at length).
-    const linePoint = useCallback((a, b, length) => {
-        let dir = b.clone().sub(a).normalize().multiplyScalar(length)
-        return a.clone().add(dir)
-    })
-
     // Scale tires.
-    useEffect(() => {
-        if (!tire) return
-
+    const tireGeometry = useMemo(() => {
         // Determine y scale as a percentage of width.
         const wheelWidth = (rim_width * 2.54) / 100
         const wheelWidthScale = wheelWidth / vehicleConfigs.wheels.tires[tire].width
@@ -93,23 +71,20 @@ const Tire = memo(({ rim_diameter, rim_width, tire, tire_diameter }) => {
             geometry.attributes.position.setY(i, setVector.y)
         }
 
-        // Update geometry.
-        geometry.attributes.position.needsUpdate = true
+        return geometry
+    }, [tireGltf.scene.children, rim_diameter, rim_width, tire, tire_diameter])
 
-        // Overwrite tire geometry.
-        tireScene.children[0].geometry = geometry
-    }, [tireScene, tireGltf.scene.children, rim_diameter, rim_width, tire, tire_diameter])
+    // Calculate rim scale as a percentage of diameter.
+    const odScale = useMemo(() => ((rim_diameter * 2.54) / 100 + 0.03175) / vehicleConfigs.wheels.rims[rim].od, [rim, rim_diameter])
 
-    // Set color.
+    // Calculate rim width.
+    const widthScale = useMemo(() => (rim_width * 2.54) / 100 / vehicleConfigs.wheels.rims[rim].width, [rim, rim_width])
+
+    // Set rim color.
     useEffect(() => {
-        setObjectMaterials(tireScene)
-    }, [setObjectMaterials, tireScene])
+        setObjectMaterials(rimGltf.scene, color, roughness, rim_color, rim_color_secondary)
+    }, [rimGltf.scene, setObjectMaterials, rim_color, rim_color_secondary, color, roughness])
 
-    return <primitive name='Tire' object={tireScene} />
-})
-
-// Wheels.
-const Wheels = memo(({ rim, rim_diameter, rim_width, rim_color, rim_color_secondary, tire, tire_diameter, offset, wheelbase, axleHeight, color, roughness }) => {
     // Build wheel transforms.
     const wheelTransforms = useMemo(() => {
         const rotation = (Math.PI * 90) / 180
@@ -126,16 +101,10 @@ const Wheels = memo(({ rim, rim_diameter, rim_width, rim_color, rim_color_second
         <group name='Wheels'>
             {wheelTransforms.map((transform) => (
                 <group {...transform}>
-                    <Rim
-                        rim={rim}
-                        rim_diameter={rim_diameter}
-                        rim_width={rim_width}
-                        rim_color={rim_color}
-                        rim_color_secondary={rim_color_secondary}
-                        color={color}
-                        roughness={roughness}
-                    />
-                    <Tire tire={tire} tire_diameter={tire_diameter} rim_diameter={rim_diameter} rim_width={rim_width} />
+                    <primitive name='Rim' object={rimGltf.scene.clone()} scale={[odScale, odScale, widthScale]} />
+                    <mesh name='Tire' geometry={tireGeometry} castShadow>
+                        <meshStandardMaterial color='#121212' />
+                    </mesh>
                 </group>
             ))}
         </group>
@@ -147,9 +116,6 @@ const Body = memo(({ id, height, color, roughness, addons, setVehicle }) => {
     const vehicle = useRef()
     const { setObjectMaterials } = useMaterialProperties()
 
-    // Load body model.
-    const vehicleGltf = useGLTF(vehicleConfigs.vehicles[id].model)
-
     // Set body color.
     useEffect(() => {
         setObjectMaterials(vehicle.current, color, roughness)
@@ -160,15 +126,13 @@ const Body = memo(({ id, height, color, roughness, addons, setVehicle }) => {
         setVehicle({ addons: vehicleConfigs.vehicles[id].default_addons })
     }, [setVehicle, id])
 
-    // Load addons.
-    const addonsModels = useMemo(() => {
+    // Build array of addon paths.
+    const addonPaths = useMemo(() => {
         return Object.entries(addons)
             .filter(([type, value]) => vehicleConfigs.vehicles[id]['addons'][type]?.['options'][value])
             .map(([type, value]) => {
-                const modelPath = vehicleConfigs.vehicles[id]['addons'][type]['options'][value]['model']
-                const model = useGLTF(modelPath)
-                model.scene.name = type + '_' + value
-                return model.scene
+                // Return path.
+                return vehicleConfigs.vehicles[id]['addons'][type]['options'][value]['model']
             })
     }, [id, addons])
 
@@ -177,12 +141,14 @@ const Body = memo(({ id, height, color, roughness, addons, setVehicle }) => {
 
     return (
         <group ref={vehicle} name='Body' key={id}>
-            <primitive name='Model' object={vehicleGltf.scene} />
-            <group name='Addons'>
-                {addonsModels.map((model) => (
-                    <primitive key={model.name} object={model} />
-                ))}
-            </group>
+            <Model path={vehicleConfigs.vehicles[id].model} />
+            {addonPaths.length ? (
+                <group name='Addons'>
+                    {addonPaths.map((addon) => (
+                        <Model key={addon} path={addon} />
+                    ))}
+                </group>
+            ) : null}
         </group>
     )
 })
