687da2ae391d6df357b6db85ce16710ca57bd784
Shane Vincent <theshanergy@gmail.com>
2023-04-02 17:07:05 -0700

    remove service worker

--- main.jsx
+++ main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 )
-
-// Register service worker for offline / install.
-serviceWorker.register()
