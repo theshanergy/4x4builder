b26345288258701cad97fe43a7da9637f20a94db
Shane Vincent <theshanergy@gmail.com>
2025-03-15 10:06:41 -0700

    add id to drawer

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -1,8 +1,9 @@
 import classNames from 'classnames'
 
-const Drawer = ({ open = true, children, className }) => {
+const Drawer = ({ open = true, children, className, id }) => {
     return (
         <nav
+            id={id}
             className={classNames(
                 'fixed bottom-0 left-0 w-full max-h-1/2 lg:w-72 lg:h-screen lg:max-h-full overflow-y-auto transition-transform duration-500 scrollbar-none',
                 {
