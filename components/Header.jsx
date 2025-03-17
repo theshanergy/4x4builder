310e790dfd392a455cd2d830b087e4a5886c088a
Shane Vincent <theshanergy@gmail.com>
2025-03-17 13:45:53 -0700

    improved header layout, horizontal centering

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,14 +3,14 @@ import GitHubIcon from '../assets/images/icons/GitHub.svg'
 
 function Header() {
     return (
-        <div id='header' className='absolute top-0 h-15 flex items-center justify-between w-full border-none z-50 text-stone-900'>
+        <div id='header' className='absolute top-0 h-15 grid grid-cols-[1fr_auto_1fr] items-stretch w-full border-none z-50 text-stone-900'>
             <div />
 
+            <div className='min-w-0 justify-self-center flex items-center justify-center'>
+                <VehicleTitle />
+            </div>
 
-
-            <VehicleTitle />
-
-            <div className='px-5'>
+            <div className='px-5 flex justify-end items-center'>
                 <a target='_blank' href='https://github.com/theshanergy/4x4builder' title='GitHub'>
                     <GitHubIcon className='icon' />
                 </a>
