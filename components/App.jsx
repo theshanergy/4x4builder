52dcd9ac0c6c6404a0bb60e9e00f691110e2e2af
Shane Vincent <theshanergy@gmail.com>
2025-12-13 14:50:57 -0800

    vehicle specific meta / structured data

--- components/App.jsx
+++ components/App.jsx
@@ -1,5 +1,7 @@
+import { useParams } from 'react-router-dom'
 import useGameStore from '../store/gameStore'
 import useVehicleFromUrl from '../hooks/useVehicleFromUrl'
+import vehicleConfigs from '../vehicleConfigs'
 
 import Header from './ui/Header'
 import Sidebar from './ui/Sidebar'
@@ -11,8 +13,10 @@ import ControlsOverlay from './ui/ControlsOverlay'
 import Chat from './ui/Chat'
 import VehicleHero from './ui/VehicleHero'
 import VehicleInfo from './ui/VehicleInfo'
+import SEO from './ui/SEO'
 
 export default function App() {
+	const params = useParams()
 	const infoMode = useGameStore((state) => state.infoMode)
 	const currentVehicle = useGameStore((state) => state.currentVehicle)
 
@@ -21,6 +25,7 @@ export default function App() {
 
 	return (
 		<div className='App'>
+			<SEO />
 			{/* Canvas wrapper */}
 			<div className={`${infoMode ? 'relative min-h-[60vh]' : 'absolute inset-0 overflow-hidden'}`}>
 				<Canvas />
