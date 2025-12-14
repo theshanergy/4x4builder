748c3e513bd8ab2bed5333d9667e617d63e6b7c7
Shane Vincent <theshanergy@gmail.com>
2025-12-13 17:20:43 -0800

    logo link

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import LogoIcon from '../../assets/images/icons/Logo.svg'
+import useGameStore from '../../store/gameStore'
 
 const Logo = () => {
+	const navigate = useNavigate()
+	const setInfoMode = useGameStore((state) => state.setInfoMode)
+
+	const handleClick = () => {
+		setInfoMode(false)
+		navigate('/')
+	}
+
 	return (
-		<div className='flex h-15 items-center gap-3 px-5 text-2xl font-light'>
+		<div className='flex h-15 items-center gap-3 px-5 text-2xl font-light cursor-pointer' onClick={handleClick}>
 			<LogoIcon className='w-8' />
 			<span>
 				<strong className='font-medium'>4x4</strong>builder
