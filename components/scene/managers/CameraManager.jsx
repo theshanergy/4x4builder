39da39cc77c6d3fca41f457b43ae8d388ce2a8dd
Shane Vincent <theshanergy@gmail.com>
2025-12-30 00:27:24 -0800

    camera distance

--- components/scene/managers/CameraManager.jsx
+++ components/scene/managers/CameraManager.jsx
@@ -199,7 +199,7 @@ const OrbitCamera = ({ followSpeed, minGroundDistance, terrainMeshesCache, lastT
 			enableDamping
 			dampingFactor={0.025}
 			minDistance={2}
-			maxDistance={24000}
+			maxDistance={24}
 			minPolarAngle={Math.PI / 6}
 			maxPolarAngle={Math.PI / 2}
 			autoRotate={cameraAutoRotate}
