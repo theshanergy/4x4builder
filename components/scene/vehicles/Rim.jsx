400af35dd44169b236ab8c6bc8148999ed4c3769
Shane Vincent <theshanergy@gmail.com>
2026-01-06 12:12:47 -0800

    rename vehicle materials hook

--- components/scene/vehicles/Rim.jsx
+++ components/scene/vehicles/Rim.jsx
@@ -2,38 +2,29 @@ import { memo, useMemo, useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 import vehicleConfigs from '../../../config/vehicles'
-import useMaterialProperties from '../../../hooks/useMaterialProperties'
+import useVehicleMaterials from '../../../hooks/useVehicleMaterials'
 import cloneWithMaterials from '../../../utils/cloneWithMaterials'
 
 // Rim component - loads and renders a single rim
 const Rim = memo(({ rim, rim_diameter, rim_width, rim_color, rim_color_secondary, color, roughness }) => {
-	const { setObjectMaterials } = useMaterialProperties()
+	const { setMaterials } = useVehicleMaterials()
 
 	// Load rim model
 	const rimGltf = useGLTF(vehicleConfigs.wheels.rims[rim].model)
 
 	// Clone rim scene with unique materials
-	const { scene: rimScene, materials: rimMaterials } = useMemo(
-		() => cloneWithMaterials(rimGltf.scene),
-		[rimGltf.scene]
-	)
+	const { scene: rimScene, materials: rimMaterials } = useMemo(() => cloneWithMaterials(rimGltf.scene), [rimGltf.scene])
 
 	// Calculate rim scale as a percentage of diameter
-	const odScale = useMemo(
-		() => ((rim_diameter * 2.54) / 100 + 0.03175) / vehicleConfigs.wheels.rims[rim].od,
-		[rim, rim_diameter]
-	)
+	const odScale = useMemo(() => ((rim_diameter * 2.54) / 100 + 0.03175) / vehicleConfigs.wheels.rims[rim].od, [rim, rim_diameter])
 
 	// Calculate rim width
-	const widthScale = useMemo(
-		() => (rim_width * 2.54) / 100 / vehicleConfigs.wheels.rims[rim].width,
-		[rim, rim_width]
-	)
+	const widthScale = useMemo(() => (rim_width * 2.54) / 100 / vehicleConfigs.wheels.rims[rim].width, [rim, rim_width])
 
 	// Set rim color
 	useEffect(() => {
-		setObjectMaterials(rimMaterials, color, roughness, rim_color, rim_color_secondary)
-	}, [rimMaterials, setObjectMaterials, rim_color, rim_color_secondary, color, roughness])
+		setMaterials(rimMaterials, color, roughness, rim_color, rim_color_secondary)
+	}, [rimMaterials, setMaterials, rim_color, rim_color_secondary, color, roughness])
 
 	return <primitive name='Rim' object={rimScene} scale={[odScale, odScale, widthScale]} />
 })
---
+++ hooks/useVehicleMaterials.js
@@ -0,0 +1,130 @@
+import { useCallback } from 'react'
+import { Color } from 'three'
+import useGameStore from '../store/gameStore'
+
+const COLORS = {
+	WHITE: new Color(1, 1, 1),
+	LIGHT_GREY: new Color(0.8, 0.8, 0.8),
+	MED_GREY: new Color(0.5, 0.5, 0.5),
+	DARK_GREY: new Color(0.2, 0.2, 0.2),
+	BLACK: new Color(0.025, 0.025, 0.025),
+	BRONZE: new Color().setHSL(0.075, 0.5, 0.35),
+	HEADLIGHT_BLUE: new Color(0.8, 0.9, 1.0),
+}
+
+// Vehicle materials.
+const useVehicleMaterials = () => {
+	const lightsOn = useGameStore((state) => state.lightsOn)
+
+	// Set object materials.
+	const setMaterials = useCallback(
+		(materials, color, roughness, rim_color, rim_color_secondary) => {
+			if (!materials || materials.size === 0) return
+
+			materials.forEach((material) => {
+				// Switch material name.
+				switch (material.name) {
+					case 'body':
+						material.color.setStyle(color)
+						material.metalness = 0.4
+						material.roughness = roughness
+						break
+					case 'chrome':
+					case 'mirror':
+						material.metalness = 1
+						material.roughness = 0
+						material.color.set(COLORS.WHITE)
+						break
+					case 'drl':
+					case 'foglight':
+					case 'headlight':
+						material.metalness = 1
+						material.roughness = 0
+						material.color.set(COLORS.WHITE)
+						material.emissive = COLORS.HEADLIGHT_BLUE
+						material.emissiveIntensity = lightsOn ? 0.9 : 0.1
+						material.toneMapped = false
+						break
+					case 'glass':
+						material.transparent = true
+						material.metalness = 1
+						material.roughness = 0
+						material.opacity = 0.2
+						material.depthWrite = true
+						material.color.set(COLORS.LIGHT_GREY)
+						break
+					case 'glass_tint':
+						material.transparent = true
+						material.metalness = 1
+						material.roughness = 0
+						material.opacity = 0.5
+						material.depthWrite = true
+						material.color.set(COLORS.BLACK)
+						break
+					case 'glass_dark':
+						material.transparent = true
+						material.metalness = 1
+						material.roughness = 0
+						material.opacity = 0.7
+						material.depthWrite = true
+						material.color.set(COLORS.BLACK)
+						break
+					case 'rubber':
+						material.metalness = 0.5
+						material.roughness = 0.9
+						material.flatShading = true
+						material.color.set(COLORS.BLACK)
+						break
+					case 'black':
+						material.metalness = 0
+						material.roughness = 0.6
+						material.color.set(COLORS.BLACK)
+						break
+					case 'rim':
+					case 'rim_secondary':
+						// Switch rim color / secondary rim color.
+						switch (material.name === 'rim_secondary' ? rim_color_secondary : rim_color) {
+							case 'silver':
+								material.metalness = 0.8
+								material.roughness = 0.3
+								material.color.set(COLORS.LIGHT_GREY)
+								break
+							case 'chrome':
+								material.metalness = 0.8
+								material.roughness = 0
+								material.color.set(COLORS.WHITE)
+								break
+							case 'gloss_black':
+								material.metalness = 1
+								material.roughness = 0.1
+								material.color.set(COLORS.BLACK)
+								break
+							case 'flat_black':
+								material.metalness = 0.3
+								material.roughness = 1
+								material.color.set(COLORS.BLACK)
+								break
+							case 'bronze':
+								material.metalness = 0.8
+								material.roughness = 0.3
+								material.color.set(COLORS.BRONZE)
+								break
+							case 'body':
+								material.metalness = 0.4
+								material.roughness = roughness
+								material.color.setStyle(color)
+								break
+							default:
+						}
+						break
+					default:
+				}
+			})
+		},
+		[lightsOn]
+	)
+
+	return { setMaterials }
+}
+
+export default useVehicleMaterials
